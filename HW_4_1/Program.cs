858ad53ad3aeea45c70c472c672635987e929d4e
Михаил Сандин <sandinm62@live.com>
2022-08-18 16:11:30 +0300

    Выполнил частично первую задачу

--- HW_4_1/Program.cs
+++ HW_4_1/Program.cs
@@ -8,26 +8,61 @@ using System.Threading.Tasks;
 
 namespace HW_4_1
 {
+    //1.
+    //а) Дописать класс для работы с одномерным массивом.
+    //Реализовать конструктор, создающий массив определенного размера и
+    //заполняющий массив числами от начального значения с заданным шагом.
+    //Создать свойство Sum, которое возвращает сумму элементов массива, метод Inverse,
+    //возвращающий новый массив с измененными знаками у всех элементов
+    //массива(старый массив, остается без изменений), метод Multi,
+    //умножающий каждый элемент массива на определённое число, свойство MaxCount,
+    //возвращающее количество максимальных элементов.
+    //б)** Создать библиотеку содержащую класс для работы с массивом.
+    //Продемонстрировать работу библиотеки
+    // 
     class MyArray
     {
         public int[] a;
+
+
         //int[] inverseMass;
         // Создание массива и заполнение его одним значением el
         public MyArray(int n, int el)
-        {   
+        {
 
             a = new int[n];
             for (int i = 0; i < n; i++)
             {
                 a[i] = el;
                 el = el + 2;
-                
-            }
-               
-        }
-   
-        
 
+            }
+
+        }
+
+        //public void inverse(int[] newInverseMass)
+        //{
+        //    Array.Copy(a, a.Length);
+        //    for (int i = 0; i < newInverseMass.Length; i++)
+        //        newInverseMass[i] *= -1;
+        //}
+
+         public int[] multi()
+        {
+
+            int[] array = new int[a.Length];
+           
+            for (int i = 0; i < a.Length; i++)
+            {
+                array[i] = a[i] * 3;
+                Console.Write(array[i] + " ");
+            }
+            
+
+
+            return array;
+            
+        }
         public int sum
         {
             get
@@ -38,7 +73,8 @@ namespace HW_4_1
                 return sum;
             }
         }
-       
+
+
         public int Max
         {
             get
@@ -59,6 +95,20 @@ namespace HW_4_1
                 return min;
             }
         }
+        public int MaxCount
+        {
+            get
+            {
+                int max = a.Max();
+                int s = 0;
+                for (int i = 0; i < a.Length; i++)
+                {
+                    if (a[i] == max)
+                        s += 1;
+                }
+                return s;
+            }
+        }
         public int CountPositiv
         {
             get
@@ -76,20 +126,22 @@ namespace HW_4_1
                 s = s + v + " ";
             return s;
         }
+
     }
 
 
     internal class Program
     {
-        //public int inverse(int[] inverseMass)
+        //public int[] inverse(int[] inverseMass)
         //{
 
-
-        //    for (int i = 0; i < a.Length; i++)
-        //        inverseMass[i] = inverseMass[i] * (-1);
-        //    return inverseMass[0];
+        //    int[] newInverseMass = new int[inverseMass.Length];
+        //    for (int i = 0; i < inverseMass.Length; i++)
+        //        newInverseMass[i] = -inverseMass[i];
+        //    return newInverseMass;
 
         //}
+
         static void Main(string[] args)
         {
             MyArray a = new MyArray(10, 1);
@@ -97,9 +149,12 @@ namespace HW_4_1
             Console.WriteLine(a.Max);
             Console.WriteLine(a.Min);
             Console.WriteLine("sum = " + a.sum);
-           // inverse(a);
-           // Console.WriteLine(inverseMass.ToString());
+            a.multi();
+            Console.WriteLine();
+            Console.WriteLine("MaxCount = " + a.MaxCount);
 
+
+            
             Console.WriteLine(a.CountPositiv);
             Console.ReadKey();
         }
